61e58359b9518b4cf285fc0c73a3fbe1447e49f9
monkey39 <1774510024@qq.com>
2017-05-22 21:51:44 +0800

    __init__.py file is firstly upload.
    Hello World!

---
+++ __init__.py
@@ -0,0 +1,26 @@
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+import url_manager
+import html_downloader
+import html_outputer
+import html_parser
+
+
+class SpiderMain(object):
+    # initiate all objects
+    def __init__(self):
+        self.urls = url_manager.UrlManager()  # url_manager
+        self.downloader = html_downloader.HtmlDownloader()  # html_downloader
+        self.parser = html_parser.HtmlParser()  # html_parser
+        self.outputer = html_outputer.HtmlOutputer()  # html_outputer
+
+    def craw(self, root_url):
+        count = 1
+        # put root_url in url_manager
+        self.urls.add_new_url(root_url)
+
+
+if __name__ == "__main__":
+    root_url = "http://baike.baidu.com/item/行政区划"  # this link is the entrance of provinces in china.
+    obj_spider = SpiderMain()
+    obj_spider.craw(root_url)  # 启动爬虫
